1ef397568dcfff34a7d2ebd4b742cadad7d3000e
Anders Christiansen Sørby <anders@sorby.xyz>
2022-03-04 23:14:55 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "Neptune";
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs;
+    flake-utils = {
+      url = github:numtide/flake-utils;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = github:nix-community/naersk;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils = {
+      url = github:yatima-inc/nix-utils;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.naersk.follows = "naersk";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , utils
+    , naersk
+    }:
+    utils.lib.eachDefaultSystem (system:
+    let
+      # Contains nixpkgs.lib, flake-utils.lib and custom functions
+      lib = utils.lib.${system};
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (lib) buildRustProject testRustProject getRust filterRustProject;
+      # Load a nightly rust. The hash takes precedence over the date so remember to set it to
+      # something like `lib.fakeSha256` when changing the date.
+      rust = getRust { date = "2022-02-20"; sha256 = "sha256-ZptNrC/0Eyr0c3IiXVWTJbuprFHq6E1KfBgqjGQBIRs="; };
+      crateName = "neptune";
+      root = ./.;
+      # This is a wrapper around naersk build
+      # Remember to add Cargo.lock to git for naersk to work
+      project = buildRustProject {
+        inherit root rust;
+      };
+    in
+    {
+      packages.${crateName} = project;
+      checks.${crateName} = testRustProject { inherit root; };
+
+      defaultPackage = self.packages.${system}.${crateName};
+
+      # To run with `nix run`
+      apps.${crateName} = flake-utils.lib.mkApp {
+        drv = project;
+      };
+
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        inputsFrom = builtins.attrValues self.packages.${system};
+        nativeBuildInputs = [ rust ];
+        buildInputs = with pkgs; [
+          rust-analyzer
+          clippy
+          rustfmt
+          emscripten
+        ];
+      };
+    });
+}
